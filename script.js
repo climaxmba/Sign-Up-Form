2b01c57e25ee7d8efdee9343d6b20fba7c8d3463
Climax Mba <climaxium150@gmail.com>
2023-04-16 17:35:35 +0100

    Add password validation

--- script.js
+++ script.js
@@ -3,13 +3,23 @@ const submtBtn = document.querySelector('button');
 let firstPass = document.getElementById('password');
 let secondPass = document.getElementById('confirm-password');
 
-// Set all input's indeterminate to true
 for (let i = 0; i < inputs.length; i++) {
+    // Set all input's indeterminate to true
     inputs[i].indeterminate = true;
-}
 
-submtBtn.addEventListener('click', () => validatePasswords());
-
-function validatePasswords() {
-    return;
+    if (inputs[i].type == 'password') {
+        inputs[i].addEventListener('keyup', () => {
+            if ((firstPass.value == secondPass.value) && firstPass.value.length >= 8) {
+                firstPass.classList.value = 'valid';
+                submtBtn.disabled = false;
+                return;
+            } else if (firstPass.value != secondPass.value) {
+                firstPass.classList.value = 'do-not-match';
+                submtBtn.disabled = true;
+            } else if (firstPass.value.length < 8) {
+                firstPass.classList.value = 'less-than-eight-char';
+                submtBtn.disabled = true;
+            }
+        });
+    }
 }

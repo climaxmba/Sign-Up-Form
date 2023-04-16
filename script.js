38d9afbffb0dc46c079e92e88758e1ed54b18231
Climax Mba <climaxium150@gmail.com>
2023-04-16 18:18:42 +0100

    Modify event listener & <div>

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 const inputs = document.getElementsByTagName('input');
 const submtBtn = document.querySelector('button');
+const container = document.getElementById('password-container');
 let firstPass = document.getElementById('password');
 let secondPass = document.getElementById('confirm-password');
 
 for (let i = 0; i < inputs.length; i++) {
-    // Set all input's indeterminate to true
-    inputs[i].indeterminate = true;
-
     if (inputs[i].type == 'password') {
         inputs[i].addEventListener('keyup', () => {
             if ((firstPass.value == secondPass.value) && firstPass.value.length >= 8) {
-                firstPass.classList.value = 'valid';
+                container.classList.value = 'valid';
                 submtBtn.disabled = false;
                 return;
             } else if (firstPass.value != secondPass.value) {
-                firstPass.classList.value = 'do-not-match';
+                container.classList.value = 'do-not-match';
                 submtBtn.disabled = true;
             } else if (firstPass.value.length < 8) {
-                firstPass.classList.value = 'less-than-eight-char';
+                container.classList.value = 'less-than-eight-char';
                 submtBtn.disabled = true;
             }
         });
